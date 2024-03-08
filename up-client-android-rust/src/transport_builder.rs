ac650e2bfdf0a398344c045adf0fa8918f091c14
Peter LeVasseur <peter.levasseur@gm.com>
2024-03-08 11:28:59 -0500

    Added UTransportBuilder impl for up-client-android-rust.

---
+++ up-client-android-rust/src/transport_builder.rs
@@ -0,0 +1,19 @@
+use std::sync::Arc;
+use binder::Strong;
+use up_rust::transport::datamodel::UTransport;
+use ustreamer::utransport_builder::UTransportBuilder;
+use aidl_rust_codegen::binder_impls::IUBus::IUBus;
+use crate::UpClientAndroid;
+
+struct AndroidTransportBuilder {
+    ubus: Arc<Strong<dyn IUBus>>,
+}
+
+impl UTransportBuilder for AndroidTransportBuilder {
+    fn build(&self) -> Box<dyn UTransport> {
+        let ubus = Arc::clone(&self.ubus);
+
+        let up_client: Box<dyn UTransport> = Box::new(UpClientAndroid::new(ubus));
+        up_client
+    }
+}
