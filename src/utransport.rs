ea8656a635f32d597fe774a8e0ce19ce51f0a56e
Peter LeVasseur <peter.levasseur@gm.com>
2024-04-11 17:19:46 -0400

    Enhanced logging inside of register_listener()

--- src/utransport.rs
+++ src/utransport.rs
@@ -1,17 +1,19 @@
-use crate::UPClientAndroid;
+use crate::{UPClientAndroid, UPCLIENTANDROID_TAG};
 use async_std::task;
 use async_trait::async_trait;
 use jni::objects::{JByteArray, JClass, JObject, JValue};
 use jni::sys::{jlong, jstring};
 use jni::JNIEnv;
 use lazy_static::lazy_static;
-use log::error;
+use log::{error, trace};
 use protobuf::Message;
 use std::collections::hash_map::DefaultHasher;
 use std::collections::{HashMap, HashSet};
 use std::hash::{Hash, Hasher};
 use std::sync::{Arc, Mutex};
 use up_rust::{ComparableListener, UCode, UListener, UMessage, UStatus, UTransport, UUri};
+
+const UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG: &str = "register_listener:";
 
 const CLASS_UURI: &str = "Lorg/eclipse/uprotocol/v1/UUri;";
 const CLASS_ULISTENER: &str = "Lorg/eclipse/uprotocol/transport/UListener;";
@@ -105,11 +107,23 @@ impl UTransport for UPClientAndroid {
         // that Java class implementing UListener would then be used to call the passed in
         // the Rust listener: Arc<dyn UListener>
 
+        trace!(
+            "{}:{} Entered register_listener",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
+
         // Get JNIEnv for the current thread
         let mut env = self
             .vm
             .attach_current_thread()
             .expect("Failed to attach current thread");
+
+        trace!(
+            "{}:{} Got JNIEnv",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
 
         // TODO: Add logic for making sure we only add listener once
         let hash = store_listener(topic.clone(), listener);
@@ -118,12 +132,30 @@ impl UTransport for UPClientAndroid {
         let listener_class = env
             .find_class("org/eclipse/uprotocol/streamer/service/UListenerNativeBridge")
             .expect("Failed to find UListenerImpl class");
+
+        trace!(
+            "{}:{} Got UListenerNativeBridge class",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
+
         let listener_obj = env
             .new_object(listener_class, "()V", &[JValue::Long(hash as jlong)])
-            .expect("Failed to create UListenerImpl object");
+            .expect("Failed to create UListenerNativeBridge object");
+
+        trace!(
+            "{}:{} Constructed UListenerNativeBridge object",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
 
         // Check if an exception occurred
         if env.exception_check().unwrap() {
+            trace!(
+                "{}:{} Hit exception constructing UListenerNativeBridge object",
+                UPCLIENTANDROID_TAG,
+                UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+            );
             // Optionally log or describe the exception
             env.exception_describe().unwrap(); // This will print the exception details to the console
             env.exception_clear().unwrap(); // Clears the exception so that JNI calls can continue
@@ -135,6 +167,11 @@ impl UTransport for UPClientAndroid {
         }
 
         let up_client_ref = self.up_client.as_obj(); // Convert GlobalRef to JObject
+        trace!(
+            "{}:{} Converted GlobalRef to JObject",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
 
         let Ok(uuri_bytes) = topic.write_to_bytes() else {
             error!("Failed to serialize UUri to bytes");
@@ -143,13 +180,28 @@ impl UTransport for UPClientAndroid {
                 "Failed to obtain UUri bytes",
             )); // Replace UStatus::Error with appropriate error handling
         };
+        trace!(
+            "{}:{} Turned UUri into byte vec",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
         let byte_array = env
             .byte_array_from_slice(&uuri_bytes)
             .expect("Couldn't create jbyteArray from Rust Vec<u8>");
+        trace!(
+            "{}:{} Turned byte vec into JByteArray",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
 
         let native_bridge_class = env
             .find_class("org/eclipse/uprotocol/streamer/service/NativeBridge")
             .expect("Couldn't find the Helper class");
+        trace!(
+            "{}:{} Found NativeBridge class",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
         let uuri_obj = env
             .call_static_method(
                 native_bridge_class,
@@ -160,9 +212,19 @@ impl UTransport for UPClientAndroid {
             .expect("Java method failed")
             .l()
             .expect("Expected a UUri object");
+        trace!(
+            "{}:{} Turned serialized JByteArray into UUri JObject",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
 
         // Check if an exception occurred
         if env.exception_check().unwrap() {
+            trace!(
+                "{}:{} Exception occured while turning JByteArray into UUri JObject",
+                UPCLIENTANDROID_TAG,
+                UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+            );
             // Optionally log or describe the exception
             env.exception_describe().unwrap(); // This will print the exception details to the console
             env.exception_clear().unwrap(); // Clears the exception so that JNI calls can continue
@@ -174,6 +236,11 @@ impl UTransport for UPClientAndroid {
         }
 
         let args = [JValue::Object(&uuri_obj), JValue::Object(&listener_obj)];
+        trace!(
+            "{}:{} Formed arguments to the Java UPClient's registerListener",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
 
         env.call_method(
             up_client_ref,
@@ -182,9 +249,19 @@ impl UTransport for UPClientAndroid {
             &args,
         )
         .expect("Unable to call UPClient.registerListner()");
+        trace!(
+            "{}:{} Called registerListener on the Java UPClient",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
 
         // Check if an exception occurred
         if env.exception_check().unwrap() {
+            trace!(
+                "{}:{} Exception occurred while calling registerListener on the Java UPClient",
+                UPCLIENTANDROID_TAG,
+                UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+            );
             // Optionally log or describe the exception
             env.exception_describe().unwrap(); // This will print the exception details to the console
             env.exception_clear().unwrap(); // Clears the exception so that JNI calls can continue
@@ -195,7 +272,13 @@ impl UTransport for UPClientAndroid {
             )); // Replace UStatus::Error with appropriate error handling
         }
 
-        todo!()
+        trace!(
+            "{}:{} Reached bottom of function",
+            UPCLIENTANDROID_TAG,
+            UPANDROIDCLIENT_FN_REGISTER_LISTENER_TAG
+        );
+
+        Ok(())
     }
 
     async fn unregister_listener(
