55640b8fc968ddfe6d6aea80efa4af63300d7f3e
Peter LeVasseur <peter.levasseur@gm.com>
2024-04-12 12:39:38 -0400

    Modified signature of UPClientAndroid::new(), may roll back...

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,15 @@ pub struct UPClientAndroid {
     vm: JavaVM,
     up_client: GlobalRef,
     usub: GlobalRef,
+    uuri_class: GlobalRef,
+    ustatus_class: GlobalRef,
+    ulistener_native_bridge_class: GlobalRef,
+    native_bridge_class: GlobalRef,
 }
 
 impl UPClientAndroid {
-    pub async fn new(vm: JavaVM, up_client: GlobalRef, usub: GlobalRef) -> Self {
+    pub async fn new(vm: JavaVM, up_client: GlobalRef, usub: GlobalRef, uuri_class: GlobalRef,
+    ustatus_class: GlobalRef, ulistener_native_bridge_class: GlobalRef, native_bridge_class: GlobalRef) -> Self {
         trace!(
             "{}:{} Able to instantiate UPClientAndroid",
             UPCLIENTANDROID_TAG,
@@ -24,6 +29,10 @@ impl UPClientAndroid {
             vm,
             up_client,
             usub,
+            uuri_class,
+            ustatus_class,
+            ulistener_native_bridge_class,
+            native_bridge_class
         }
     }
 }
